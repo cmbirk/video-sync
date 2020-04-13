6335d22f3516e4b8b81e48ed445e3d847a4af76a
Chris Birk <cmbirk@gmail.com>
2020-04-12 22:46:27 -0500

    add storybook

--- packages/video-sync-nextjs/src/layout/Layout.jsx
+++ packages/video-sync-nextjs/src/layout/Layout.jsx
@@ -3,22 +3,21 @@ import PropTypes from 'prop-types'
 // import Footer from './Footer'
 import Header from '@layout/Header'
 
-const Layout = ({ children }) =>
+const Layout = ({ children, hasSidebar = false }) =>
   (
     <>
       <Header />
       <div
         id="wrapper"
         className="container mx-auto flex flex-col relative justify-center py-10 bg-gray-200">
-        {/* <Header /> */}
         <main className="text-gray-900 mb-4">{children}</main>
-        {/* <Footer /> */}
       </div>
     </>
   )
 
 Layout.propTypes = {
   children: PropTypes.node,
+  hasSidebar: PropTypes.bool,
 }
 
 export default Layout
